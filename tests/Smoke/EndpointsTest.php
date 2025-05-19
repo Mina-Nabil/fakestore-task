3aa2343f34ec1135bf50cc535afc60d707e3a11b
Mina Nabil <mina9492@hotmail.com>
2025-05-19 22:17:21 +0300

    adding smoke testing to backend

---
+++ tests/Smoke/EndpointsTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Smoke;
+
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+
+class EndpointsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->artisan('migrate');
+    }
+
+    /**
+     * A basic test example.
+     */
+    public function test_login_endpoint(): void
+    {
+        $response = $this->post('/api/login', [
+            'username' => 'test',
+            'password' => 'test',
+        ]);
+
+        $response->assertStatus(404);
+    }
+
+    public function test_products_endpoint(): void
+    {
+        $response = $this->get('/api/products');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_update_product_endpoint(): void
+    {
+        Product::create([
+            'title' => 'test',
+            'price' => 10,
+            'category' => 'test',
+            'description' => 'test',
+            'image' => 'test',
+        ]);
+
+        $response = $this
+        ->actingAs(User::factory()->create())
+        ->put('/api/products/1', [
+            'title' => 'test',
+            'price' => 10,
+            'description' => 'test',
+            'image' => 'test',
+        ]);
+
+        $response->assertStatus(200);
+    }
+}
